b7a9faae18ce0f8e8f54d94232390b5c78b0a9a5
vineetkrishnagupta <vineetkrishnagupta@gmail.com>
2026-09-25 15:07:51 +0530

    work on setting theam

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,17 +9,17 @@ export default {
     extend: {
       colors: {
         primary: {
-          50: 'rgb(var(--color-primary-50, 239 246 255) / <alpha-value>)',
-          100: 'rgb(var(--color-primary-100, 219 234 254) / <alpha-value>)',
-          200: 'rgb(var(--color-primary-200, 191 219 254) / <alpha-value>)',
-          300: 'rgb(var(--color-primary-300, 147 197 253) / <alpha-value>)',
-          400: 'rgb(var(--color-primary-400, 96 165 250) / <alpha-value>)',
-          500: 'rgb(var(--color-primary-500, 59 130 246) / <alpha-value>)',
-          600: 'rgb(var(--color-primary-600, 37 99 235) / <alpha-value>)',
-          700: 'rgb(var(--color-primary-700, 29 78 216) / <alpha-value>)',
-          800: 'rgb(var(--color-primary-800, 30 64 175) / <alpha-value>)',
-          900: 'rgb(var(--color-primary-900, 30 58 138) / <alpha-value>)',
-          950: 'rgb(var(--color-primary-950, 23 37 84) / <alpha-value>)',
+          50: 'rgb(var(--color-primary-50) / <alpha-value>)',
+          100: 'rgb(var(--color-primary-100) / <alpha-value>)',
+          200: 'rgb(var(--color-primary-200) / <alpha-value>)',
+          300: 'rgb(var(--color-primary-300) / <alpha-value>)',
+          400: 'rgb(var(--color-primary-400) / <alpha-value>)',
+          500: 'rgb(var(--color-primary-500) / <alpha-value>)',
+          600: 'rgb(var(--color-primary-600) / <alpha-value>)',
+          700: 'rgb(var(--color-primary-700) / <alpha-value>)',
+          800: 'rgb(var(--color-primary-800) / <alpha-value>)',
+          900: 'rgb(var(--color-primary-900) / <alpha-value>)',
+          950: 'rgb(var(--color-primary-950) / <alpha-value>)',
         },
         sidebar: {
           bg: '#0f172a',
